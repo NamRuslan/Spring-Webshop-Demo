9f53f19bfd2e5c0ba0647918aab8109e4f9a788b
NamRuslan <rus.nam.96@bk.ru>
2021-04-06 18:12:42 +0300

    Created project

---
+++ src/main/java/com/namruslan/springwebshopdemo/SpringWebShopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.namruslan.springwebshopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebShopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebShopDemoApplication.class, args);
+	}
+
+}
